a5944c9bd8e871b7c0f0732e3e264fa4443752d7
BioSLuDge <biosludge@gmail.com>
2020-08-18 20:19:13 -0600

    inital commit

--- weatherStation_config.h
+++ weatherStation_config.h
@@ -3,40 +3,37 @@
 
 #define WIND_DIR_OFFSET 0
 
-#define WLAN_SSID       "YOUR WIFI SSID"
-#define WLAN_PASS       "YOUR WIFI PASSWORD"
+#define IO_LOOP_DELAY (1000*60*2) //every 2 minute
 
-#define AIO_SERVER      "MQTT HOSTNAME"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "MQTT USERNAME"
-#define AIO_KEY         "MQTT PASSWORD"
+#define IO_RESET_FEED       "reset"
+#define IO_WINDSPEEDMPH_FEED "wind-speed"
+#define IO_WINDGUSTMPH_FEED "wind-gust"
+#define IO_WINDSPDMPH_AVG2M_FEED "wind-speed-2-minute-average"
+#define IO_WINDGUSTMPH_10M_FEED "wind-gust-10-minute-average"
+#define IO_RAININ_FEED "rain"
+#define IO_DAILYRAININ_FEED "rain-daily"
+#define IO_WINDDIR_FEED "wind-direction"
+#define IO_WINDGUSTDIR_FEED "wind-gust-direction"
+#define IO_WINDDIR_AVG2M_FEED "wind-direction-2-minute-average"
+#define IO_WINDGUSTDIR_10M_FEED "wind-gust-direction-10-minute-average"
+#define IO_BATTERY_FEED "battery-power"
+#define IO_DONE_FEED "battery-charged"
+#define IO_CHARGED_FEED "charged"
+#define IO_SOLAR_FEED "solar"
+#define IO_VISIBILITY_FEED "visibility"
+#define IO_IR_FEED "ir"
+#define IO_DEWPT_FEED "dew-point"
+#define IO_HUMIDITY_FEED "humidity"
+#define IO_UV_FEED "uv"
+#define IO_BAROMIN_FEED "baromin"
+#define IO_TEMP_FEED "temperature"
+#define IO_TEMP2_FEED "temperature-2"
+#define IO_PRESSURE_FEED "pressure"
+#define IO_ALTITUDE_FEED "altitude"
 
-#define AIO_RESET_FEED       "myfeed/reset"
-#define AIO_READ_FEED        "myfeed/read"
-#define AIO_WINDSPEEDMPH_FEED "myfeed/windspeedmph";
-#define AIO_WINDGUSTMPH_FEED "myfeed/windgustmph";
-#define AIO_WINDSPDMPH_AVG2M_FEED "myfeed/windspdmph_avg2m";
-#define AIO_WINDGUSTMPH_10M_FEED "myfeed/windgustmph_10m";
-#define AIO_RAININ_FEED "myfeed/rainin";
-#define AIO_DAILYRAININ_FEED "myfeed/dailyrainin";
-#define AIO_WINDDIR_FEED "myfeed/winddir";
-#define AIO_WINDGUSTDIR_FEED "myfeed/windgustdir";
-#define AIO_WINDDIR_AVG2M_FEED "myfeed/winddir_avg2m";
-#define AIO_WINDGUSTDIR_10M_FEED "myfeed/windgustdir_10m";
-#define AIO_BATTERY_FEED "myfeed/battery";
-#define AIO_DONE_FEED "myfeed/done";
-#define AIO_CHARGE_FEED "myfeed/charge";
-#define AIO_SOLAR_FEED "myfeed/solar";
-#define AIO_VISIBILITY_FEED "myfeed/visibility";
-#define AIO_IR_FEED "myfeed/ir";
-#define AIO_DEWPT_FEED "myfeed/dewpt";
-#define AIO_HUMIDITY_FEED "myfeed/humidity";
-#define AIO_UV_FEED "myfeed/uv";
-#define AIO_BAROMIN_FEED "myfeed/baromin";
-#define AIO_TEMP_FEED "myfeed/temp";
-#define AIO_TEMP2_FEED "myfeed/temp2";
-#define AIO_PRESSURE_FEED "myfeed/pressure";
-#define AIO_ALTITUDE_FEED "myfeed/altitude";
+#define IO_LATITUDE 1
+#define IO_LONGITUDINAL 1
+#define IO_ELEVATION 1
 
-#define FLOAT_PERSISION 4
+#define IO_PERSISION 4
 #endif
